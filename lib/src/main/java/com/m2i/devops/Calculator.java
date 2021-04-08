5ca4612712f4d713180cfb9a5280a9edb08d0e25
FrankieLeMalhonnete <chopin.fabrice@gmail.com>
2021-04-08 16:03:15 +0200

    Javadoc and correct Checkstyle config

--- lib/src/main/java/com/m2i/devops/Calculator.java
+++ lib/src/main/java/com/m2i/devops/Calculator.java
@@ -3,38 +3,82 @@ package com.m2i.devops;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Calculator engine class.
+ * Provides few functions...
+ * 
+ * @author Frankie
+ * @version 1.0
+ */
 public class Calculator {
 
-	// Triggers Spotbug error report
-	public Object toto = null;
-	
+	/**
+	 * Function Add two values.
+	 * 
+	 * @param a first value
+	 * @param b second value
+	 * @return result of addition
+	 */
 	public int add(int a, int b) {
-		return a+b;
+		return a + b;
 	}
 	
+	/**
+	 * Function Subtract two values.
+	 * 
+	 * @param a first value
+	 * @param b second value
+	 * @return result of subtraction
+	 */
 	public int sub(int a, int b) {
-		return a-b;
+		return a - b;
 	}
 	
+	/**
+	 * Function Multiply two values.
+	 * 
+	 * @param a first value
+	 * @param b second value
+	 * @return result of multiplication
+	 */
 	public int mult(int a, int b) {
-		return a*b;
+		return a * b;
 	}
 	
+	/**
+	 * Function Divide two values.
+	 * 
+	 * @param a first value
+	 * @param b second value
+	 * @return result of division
+	 */
 	public int div(int a, int b) {
-		return a/b;
+		return a / b;
 	}
 	
+	/**
+	 * Check if value is Pair.
+	 * 
+	 * @param a value to check
+	 * @return True if pair
+	 */
 	public boolean isPair(int a) {
 		if (a%2 == 0) return true;
 		else return false;
 	}
 	
+	/**
+	 * Returns a list of prime numbers until the provided value.
+	 * 
+	 * @param value the max value to get prime numbers
+	 * @return a list of prime numbers
+	 */
 	public List<Integer> showPrimeNumbersUntil(int value) {
 		List<Integer> primeNumbers = new ArrayList<Integer>();
 		boolean flagNotPrime = false;
 		
 		for (int curs = 2 ; curs <= value ; curs++) {
-			for (int i=2 ; i <= curs / 2 ; ++i) {
+			for (int i = 2; i <= curs / 2; ++i) {
 				if (curs % i == 0) {
 					flagNotPrime = true;
 					break;
--- lib/src/test/java/com/m2i/devops/CalculatorTest.java
+++ lib/src/test/java/com/m2i/devops/CalculatorTest.java
@@ -1,6 +1,5 @@
 package com.m2i.devops;
 
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -17,10 +16,10 @@ public class CalculatorTest {
 	public void testAdd() {
 		int a = 2;
 		int b = 3;
-		
+
 		Calculator calc = new Calculator();
 		int actual = calc.add(a, b);
-		
+
 		assertEquals(5, actual);
 	}
 
@@ -28,10 +27,10 @@ public class CalculatorTest {
 	public void testSub() {
 		int a = 6;
 		int b = 3;
-		
+
 		Calculator calc = new Calculator();
 		int actual = calc.sub(a, b);
-		
+
 		assertEquals(3, actual);
 	}
 
@@ -39,10 +38,10 @@ public class CalculatorTest {
 	public void testMult() {
 		int a = 5;
 		int b = 4;
-		
+
 		Calculator calc = new Calculator();
 		int actual = calc.mult(a, b);
-		
+
 		assertEquals(20, actual);
 	}
 
@@ -50,31 +49,32 @@ public class CalculatorTest {
 	public void testDiv() {
 		int a = 15;
 		int b = 5;
-		
+
 		Calculator calc = new Calculator();
 		int actual = calc.div(a, b);
-		
+
 		assertEquals(3, actual);
 	}
 
 	@Test
 	public void testIsPair() {
 		Calculator calc = new Calculator();
-		
+
 		boolean actual = calc.isPair(21);
 		assertFalse(actual);
-		
+
 		actual = calc.isPair(10);
 		assertTrue(actual);
 	}
 
 	@Test
 	public void testShowPrimeNumbersUntil() {
-		List<Integer> firstFivePrimeNumbers = new ArrayList<Integer> (Arrays.asList(2,3,5,7,11));
+		List<Integer> firstFivePrimeNumbers = new ArrayList<Integer>(Arrays.asList(2, 3, 5, 7, 11));
 		Calculator calc = new Calculator();
-		
+
 		List<Integer> returnedPrimeNumbersUntil = calc.showPrimeNumbersUntil(12);
-		
-		assertTrue(firstFivePrimeNumbers.size() == returnedPrimeNumbersUntil.size() && firstFivePrimeNumbers.containsAll(returnedPrimeNumbersUntil));
+
+		assertTrue(firstFivePrimeNumbers.size() == returnedPrimeNumbersUntil.size()
+				&& firstFivePrimeNumbers.containsAll(returnedPrimeNumbersUntil));
 	}
 }
